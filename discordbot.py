0b14ee5876ad20615ad3f61c879a1c75302cdc0a
TKRMGM <66470753+TKRMGM@users.noreply.github.com>
2020-06-05 21:54:45 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -21,7 +21,7 @@
 # RUN THE BOT (run from command line)
 #
 # python3 intervalbot.py
-
+"""
 import discord
 import asyncio
 
@@ -95,7 +95,7 @@ async def on_ready():
 
 client.run(TOKEN)
 
-"""
+
 from discord.ext import commands
 import os
 import traceback
@@ -122,3 +122,34 @@ async def pping(ctx):
 
 bot.run(token)
 """
+
+
+from discord.ext import tasks
+import discord
+import os
+
+token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
+
+channel_sent = None
+"""
+10秒ごとに発言するメソッドを定義している部分。
+async def の1行上が定期実行を示すもので、()内で間隔を指定します。
+例えば5分ごとなら(minutes=5)です。
+"""
+@tasks.loop(seconds=10)
+async def send_message_every_10sec():
+    await channel_sent.send("10秒経ったよ")
+
+"""
+今回はbotの起動直後に定期実行を開始したいので、
+botの準備ができた段階で定期実行をstart()します
+"""
+@client.event
+async def on_ready():
+    global channel_sent 
+    channel_sent = client.get_channel(any_channel_id)
+    send_message_every_10sec.start() #定期実行するメソッドの後ろに.start()をつける
+
+
+client.run(token)
