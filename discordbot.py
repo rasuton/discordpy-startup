bc62a6d3053b0ac2cc8b15aea1d68686654908fe
TKRMGM <66470753+TKRMGM@users.noreply.github.com>
2020-06-05 21:09:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -36,11 +36,7 @@ async def reply(message):
     reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
     await message.channel.send(reply) # 返信メッセージを送信
 
-# 発言時に実行されるイベントハンドラを定義
-@client.event
-async def on_message(message):
-    if client.user in message.mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
+
 
     
 @tasks.loop(seconds=10)
@@ -49,6 +45,10 @@ async def send_message_every_10sec():
     await channel_sent.send("10秒経ったよ")
     
 @client.event
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+        await reply(message) # 返信する非同期関数を実行
+        
 async def on_ready():
     global channel_sent 
     print("これ見えてる？")
