cfaefcad4d6ef4fa4222e1aba6b6d728e91a7584
TKRMGM <66470753+TKRMGM@users.noreply.github.com>
2020-06-05 20:53:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -31,6 +31,17 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     """
+# 返信する非同期関数を定義
+async def reply(message):
+    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
+
+# 発言時に実行されるイベントハンドラを定義
+@client.event
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+        await reply(message) # 返信する非同期関数を実行
+
     
 @tasks.loop(seconds=10)
 async def send_message_every_10sec():
