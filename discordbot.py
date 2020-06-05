8f8d7e6e624f51d9a11e2fe255b8997bcb73b71d
TKRMGM <66470753+TKRMGM@users.noreply.github.com>
2020-06-05 20:33:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,22 +3,25 @@ import discord
 from discord.ext import tasks
 from discord.ext import commands
 from datetime import datetime 
+from tasks import loop
+from asyncio import sleep
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-client = discord.Client()
+
 
 target_channel_id = "471251063715004420"
 
+"""
 async def my_task():
     while True:
         # do something
         message_channel = bot.get_channel(target_channel_id)
         print(f"Got channel {message_channel}")
         await message_channel.send("Your message")
-        await asyncio.sleep(10)
+        await asyncio.sleep(10)"""
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -38,9 +41,18 @@ async def loop():
     await message_channel.send("Your message")
     await asyncio.sleep(60)
     """
-@client.command()
+"""@client.command()
 async def info():
-    client.loop.create_task(my_task())
+    client.loop.create_task(my_task())"""
 
-loop.start()
+@loop(seconds=10)
+async def name_change():
+    message_channel = bot.get_channel(target_channel_id)
+    print(f"Got channel {message_channel}")
+    await message_channel.send("Your message")
+    await sleep(10)
+    await message_channel.send("Your message")
+
+name_change.before_loop(bot.wait_until_ready())    
+name_change.start()
 bot.run(token)
