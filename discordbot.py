3ec5838678d268461cc048e2a52305d517694b1d
rasuton <66520570+rasuton@users.noreply.github.com>
2020-06-06 22:30:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,15 +10,16 @@ async def on_ready():
     asyncio.ensure_future(greeting_gm())
 
 async def greeting_gm():
-    channel = client.get_channel('485334545608998912')
+    channel = client.get_channel('718811732243382345')
     while True:
         print(datetime.datetime.now().minute)
         if(datetime.datetime.now().minute==31):
             print("eeee")
-            await client.send_message(channel, 'おはよう' + str(datetime.datetime.now()))
+            await client.send_message(channel, 'イキスギィ～' + str(datetime.datetime.now()))
+            //待つ秒数
             await asyncio.sleep(55)
         elif(datetime.datetime.now().minute==32):
-            await client.send_message(channel, 'gtegegeegう' + str(datetime.datetime.now()))
+            await client.send_message(channel, 'ハメ～' + str(datetime.datetime.now()))
             await asyncio.sleep(55)
         else:
             await asyncio.sleep(55)
